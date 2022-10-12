f041813adf5438b612c248e7c1cbbc841f7ead74
Victorakinwunmi <Adexvictor94@gmail.com>
2022-10-12 11:43:45 -0500

    i added more events

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,13 +1,83 @@
 <template>
   <div>
      <HeaderLogoMenu/>
-      <div class="group bg-no-repeat bg-cover bg-opacity-25 " :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/img2.jpg')+ ')' }">
+      <div class="group bg-no-repeat bg-cover  " :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/img2.jpg')+ ')' }">
         <div class=" bg-gradient-to-br from-primary-100 px-8 md:px-16 bg-opacity-80 lg:px-28 pt-56 pb-16 lg:pb-32 lg:pt-56">
           <div class="max-w-2xl lg:max-w-4xl bg-white bg-opacity-40 rounded-xl p-10">
             <h1 class="text-2xl lg:text-5xl text-secondary-100 font-bold">Our past campaigns have enabled us to</h1>
              <p class="text-base lg:text-xl mt-6 text-secondary-100">* Promote teaching as first profession at all levels</p>
              <p class="text-base lg:text-xl mt-6 text-secondary-100">* Bring out the best in students academic performance , promote and boost their confidence level and encapsulating the I can do aura necessary for success.</p>
           </div>
+        </div>
+      </div>
+
+      <div class="px-8 py-4 lg:px-28 lg:py-10">
+        <div class="p-4 lg:px-16 lg:py-16 bg-gray-100 rounded-xl">
+         <div class="mb-8">
+           <h1 class="font-bold mb-4 text-5xl text-secondary-100">2017</h1>
+           <p>In 2017, during our tour of  some growing schools in Iwo land, we came across Karima Ambali, an Orphan with a great academic potentiality. We paid her school fees and render other soft succour to her some other times. It was indeed fulfilling..We make a tour to the school back again around 2017 and inspired the students and have them books for self improvement.</p>
+         </div>
+         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:grid-cols-3">
+             <div class="group bg-no-repeat bg-cover bg-center h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/img4.jpg')+ ')' }"></div>
+             <div class="group bg-no-repeat bg-cover h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/orphan.jpg')+ ')' }"></div>
+             <div class="group bg-no-repeat bg-cover h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/karima2.jpg')+ ')' }"></div>
+         </div>
+        </div>
+      </div>
+
+      <div class="px-8 py-4 lg:px-28 lg:py-10">
+        <div class="p-4 lg:px-16 lg:py-16 bg-gray-100 rounded-xl">
+         <div class="mb-8">
+           <h1 class="font-bold mb-4 text-5xl text-secondary-100">2018</h1>
+           <p>In 2018,we visited Baptist high school and donated books, school bags  and scholarships to indigent students and reward Late Baba Owojori L. O and another female teacher as our teachers of the year with an award plaques. it was indeed fulfilling.</p>
+         </div>
+         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:grid-cols-3">
+             <div class="group bg-no-repeat bg-cover bg-center h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/highschl.jpg')+ ')' }"></div>
+             <div class="group bg-no-repeat bg-cover bg-center h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/highschl2.jpg')+ ')' }"></div>
+             <div class="group bg-no-repeat bg-cover h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/highschl.jpg')+ ')' }"></div>
+         </div>
+        </div>
+      </div>
+
+      <div class="px-8 py-4 lg:px-28 lg:py-10">
+        <div class="p-4 lg:px-16 lg:py-16 bg-gray-100 rounded-xl">
+         <div class="mb-8">
+           <h1 class="font-bold mb-4 text-5xl text-secondary-100">2019</h1>
+           <p>In 2019, we visited Aipate Baptist Church Grammar school and donated over 300 customised books and school bags for the junior section while we engage them with life changing seminars</p>
+         </div>
+         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:grid-cols-3">
+             <div class="group bg-no-repeat bg-cover bg-center h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/aipate2.jpg')+ ')' }"></div>
+             <div class="group bg-no-repeat bg-cover h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/aipate3.jpg')+ ')' }"></div>
+             <div class="group bg-no-repeat bg-cover h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/aipate.jpg')+ ')' }"></div>
+         </div>
+        </div>
+      </div>
+      
+      <div class="px-8 py-4 lg:px-28 lg:py-10">
+        <div class="p-4 lg:px-16 lg:py-16 bg-gray-100 rounded-xl">
+         <div class="mb-8">
+           <h1 class="font-bold mb-4 text-5xl text-secondary-100">2020</h1>
+           <p>In 2020 we visited Iwo Grammar school and donated over 500 customised books, school bags and organized quiz competition while our teacher of the year was Baba kunle Lawal who is a formal principal of the schoolcustomised books and school bags for the junior section while we engage them with life changing seminars</p>
+         </div>
+         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:grid-cols-3">
+             <div class="group bg-no-repeat bg-cover bg-center h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/img6.jpg')+ ')' }"></div>
+             <div class="group bg-no-repeat bg-cover h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/img5.jpg')+ ')' }"></div>
+             <div class="group bg-no-repeat bg-cover h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/iwogram.jpg')+ ')' }"></div>
+         </div>
+        </div>
+      </div>
+
+      <div class="px-8 py-4 lg:px-28 lg:py-10">
+        <div class="p-4 lg:px-16 lg:py-16 bg-gray-100 rounded-xl">
+         <div class="mb-8">
+           <h1 class="font-bold mb-4 text-5xl text-secondary-100">2021</h1>
+           <p>In 2021,The Renaissance instructors conference was set up at A1 comprehensive Grammar school with the theme Total education and talks like effects of drug Abuse by Dr. Abolude while Mr Asifat omowumi Moronkeji talked about entrepreneur and students. Elder Amos Idowu, Deacon Victor Idowu and professor Lasisi  were our teachers of the year. Students were given awards , bags and other soft items.</p>
+         </div>
+         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:grid-cols-3">
+             <div class="group bg-no-repeat bg-cover bg-center h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/img3.jpg')+ ')' }"></div>
+             <div class="group bg-no-repeat bg-cover h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/a1.jpg')+ ')' }"></div>
+             <div class="group bg-no-repeat bg-cover h-72" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' +  require('@/assets/image/a12.jpg')+ ')' }"></div>
+         </div>
         </div>
       </div>
   </div>
