7ff0ffe1809dcb9d4df5b6d18e97681aeb75b185
Victorakinwunmi <Adexvictor94@gmail.com>
2022-10-30 14:03:04 -0500

    i edited the tour events scaling effect

--- components/TourEvent.vue
+++ components/TourEvent.vue
@@ -1,14 +1,18 @@
 <template>
-    <div class="bg-no-repeat bg-cover scale mb-2 transition duration-500 ease-in-out lg:mb-0 bg-top rounded-xl cursor-pointer" :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' + tour_event.picture + ')' } ">
-        <div  @click="$emit('is_clicked', tour_event)" class="relative z-0 py-44 hover:bg-black hover:bg-opacity-50 transition duration-500 ease-in-out bg-gradient-to-b from-transparent to-gray-900 rounded-xl">
-            <div class="text-white absolute bottom-0 py-4 px-8 md:px-2 w-full h-24 block">
-                    <div class="w-full">
-                        <p class="text-base w-full truncate">{{tour_event.text}}</p>
-                        <button class="text-sm border mt-2  py-1 px-2 rounded-md hover:bg-gray-400 hover:border-gray-400 duration-500 ease-in-out ">Read more</button>
-                    </div>
+   <div class="relative group flex items-end rounded-2xl overflow-hidden z-0 cursor-pointer bg-gradient-to-b from-transparent to-gray-900 rounded-xl"  @click="$emit('is_clicked', tour_event)" >
+        <div class="py-44 bg-gradient-to-b w-full from-transparent to-gray-900"></div>
+        <div class="h-full absolute w-full bg-cover scale bg-center overflow-hidden transition duration-1000 ease-in-out rounded-xl cursor-pointer "
+                :style="{ backgroundRepeat: 'no-repeat' + ';', backgroundImage: 'url(' + tour_event.picture+ ')' }">
+                 <div class="py-44 bg-gradient-to-b w-full from-transparent transition duration-500 ease-in-out hover:bg-black hover:bg-opacity-50 to-gray-900"></div>
+        </div>
+        <div class="">
+            <div
+                class="rounded-2xl bg-gradient-to-b w-full from-transparent text-white to-gray-900  px-1 py-4  bottom-0 w-full  h-full duration-500 ease-in-out transform ">
+                <div class="font-medium border-l-2 border-white text-base px-2 w-72 truncate">{{tour_event.text}}</div>
+                <button class="mt-2 border rounded-xl p-2 text-sm border-white hover:bg-gray-500 duration-500 ease-in-out ">Read more</button>
             </div>
         </div>
-    </div>
+   </div>
 </template>
 
 <script>
@@ -23,6 +27,6 @@ export default {
 
 <style>
  .scale:hover{
-    transform: scale(1.1);
+    transform: scale(1.3);
  }
 </style>
